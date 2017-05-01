369d5288963d620aece2e2177d46644fe5965708
Diego Lopes <dihgg@dihgg.com>
2017-04-30 23:05:23 -0300

    Criação de uma BaseScreen

---
+++ core/src/com/pigletrun/dihgg/game/components/BaseScreen.java
@@ -0,0 +1,61 @@
+package com.pigletrun.dihgg.game.components;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class BaseScreen implements Screen {
+	// Variáveis
+	protected Game game; // a variável game (importante para o sistema)
+	protected Stage stage; // a variável de 'stage'
+	private SpriteBatch batch; // o 'batch' de desenho
+
+	public BaseScreen(final Game game) {
+		this.game = game; // Atribuir a variavel de game
+		batch = new SpriteBatch(); // iniciar o 'spritebatch'
+		stage = new Stage(); // iniciar o 'stage'
+	}
+
+
+	@Override
+	public void show() {
+
+	}
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		stage.act(Gdx.graphics.getDeltaTime());
+		stage.draw();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void hide() {
+
+	}
+
+	@Override
+	public void dispose() {
+		batch.dispose();
+		stage.dispose();
+	}
+}
--- core/src/com/pigletrun/dihgg/game/screens/IntroScreen.java
+++ core/src/com/pigletrun/dihgg/game/screens/IntroScreen.java
@@ -1,31 +1,23 @@
 package com.pigletrun.dihgg.game.screens;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Action;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.pigletrun.dihgg.game.components.BaseScreen;
 import com.pigletrun.dihgg.game.components.Logo;
 
 import java.util.ArrayList;
 
 
-public class IntroScreen implements Screen {
+public class IntroScreen extends BaseScreen {
 	// VARIÁVEIS
-	private Game game; // a variável game (importante para o sistema)
-	private SpriteBatch batch; // o 'batch' de desenho
-	private Stage stage; // a variável de 'stage'
 	private String files[] = {"unicluster.png", "unip.png", "guaxinim.png", "game.png"}; // nomes dos arquivos de logo
 	private Logo logo; // Classe de logo
 	private float delay = 2f; // tempo de delay entre os logos
 
 	public IntroScreen(final Game game) {
-		this.game = game; // Atribuir a variavel de game
-		batch = new SpriteBatch(); // iniciar o 'spritebatch'
-		stage = new Stage(); // iniciar o 'stage'
+		super(game);
+
 		logo = new Logo(files[0]); // iniciar o logo 'com o primeiro logo'
 		stage.addActor(logo); // Adicionar o ator 'logo' ao stage
 
@@ -50,49 +42,13 @@ public class IntroScreen implements Screen {
 				return true;
 			}
 		});
-		// transformar em um array primitivo de actions
+
+		// Transformar em um array primitivo de actions
 		Action[] _actions = new Action[actions.size()];
 		for (int i = 0; i < actions.size(); i++) {
 			_actions[i] = actions.get(i);
 		}
+
 		logo.addAction(Actions.sequence(_actions)); // adicionar a sequência de ações ao logo
-	}
-
-	@Override
-	public void show() {
-	}
-
-	@Override
-	public void render(float delta) {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		stage.act(Gdx.graphics.getDeltaTime());
-		stage.draw();
-	}
-
-	@Override
-	public void resize(int width, int height) {
-
-	}
-
-	@Override
-	public void pause() {
-
-	}
-
-	@Override
-	public void resume() {
-
-	}
-
-	@Override
-	public void hide() {
-
-	}
-
-	@Override
-	public void dispose() {
-		batch.dispose();
-		stage.dispose();
 	}
 }
