71f26c018b549090a0c4b2ae4d601dbe855b72cb
Diego Lopes <dihgg@dihgg.com>
2017-05-07 02:32:55 -0300

    Criação de tela de menu

--- core/src/com/pigletrun/dihgg/game/screens/MenuScreen.java
+++ core/src/com/pigletrun/dihgg/game/screens/MenuScreen.java
@@ -8,16 +8,32 @@ import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.pigletrun.dihgg.game.components.BaseScreen;
 import com.pigletrun.dihgg.game.components.ui.Button;
 
+import java.util.ArrayList;
+
 class MenuScreen extends BaseScreen {
-	private Button playBtn;
+	// VARIÁVEIS
+	private Button playBtn, optionsBtn, tutorialBtn, rankingsBtn, exitBtn; // Botões
+	private String[] files = {"play", "options", "tutorial", "rankgings", "exit"}; // lista de nomes dos botões
+	private ArrayList<Button> buttons = new ArrayList<Button>(); // array de botões
 
 	MenuScreen(final Game game) {
 		super(game);
 
-		playBtn = new Button(new Texture(Gdx.files.internal("images/ui/buttons/play-background.png"))); // criar um novo botão
-		playBtn.toCenter(); // colocá-lo no centro da tela
+		// para cada nome de botão
+		for (int i = 0; i < files.length; i++) {
+			String file = files[i]; // pegar o nome atual
+			buttons.add(new Button(new Texture(Gdx.files.internal("images/ui/buttons/" + file + "-background.png")))); // criar um botão e adicioná-lo ao array de botões
+			buttons.get(i).toCenter(0, -((buttons.get(i).getHeight() + 24) * i)); // posicioná-lo de acordo
+			stage.addActor(buttons.get(i)); // adicionar ao stage
+		}
 
-		stage.addActor(playBtn); // adicionar ao stage
+		// Atribuir cada botão a sua respectiva variável
+		// TODO: Ver como melhorar isso
+		playBtn = buttons.get(0);
+		optionsBtn = buttons.get(1);
+		tutorialBtn = buttons.get(2);
+		rankingsBtn = buttons.get(3);
+		exitBtn = buttons.get(4);
 
 		// LISTENERS
 		// Ao clicar no botão de início
