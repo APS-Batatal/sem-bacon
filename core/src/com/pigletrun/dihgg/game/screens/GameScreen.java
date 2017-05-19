d01e3fd7520fc6e4f34a1b991b0bf11926897dea
silmara <silmaraquel@ig.com.br>
2017-05-19 17:08:54 -0300

    Alterado método Listener de manipulação do porco

--- core/src/com/pigletrun/dihgg/game/screens/GameScreen.java
+++ core/src/com/pigletrun/dihgg/game/screens/GameScreen.java
@@ -1,8 +1,9 @@
 package com.pigletrun.dihgg.game.screens;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.utils.DragListener;
 import com.pigletrun.dihgg.game.components.characters.Pig;
 import com.pigletrun.dihgg.game.core.BaseScreen;
 
@@ -14,15 +15,17 @@ class GameScreen extends BaseScreen {
 		super(game); // chamar construtor pai
 
 		pig = new Pig(); // criar novo ator do porco
+		pig.setPosition(50, Gdx.graphics.getHeight() / 2);  // Define posição inicial do porco
 
 		stage.addActor(pig); // Adicionar o porco ao cenário
 
 		// LISTENERS
-		stage.addListener(new InputListener(){
+		stage.addListener(new DragListener() {
 			@Override
-			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-				pig.setPosition(x, y);
-				return true;
+			public void touchDragged(InputEvent event, float x, float y, int pointer) {
+				super.touchDragged(event, x, y, pointer);
+				if (y > 0 && y < Gdx.graphics.getHeight() - pig.getHeight())
+					pig.setPosition(50, y);
 			}
 		});
 	}
