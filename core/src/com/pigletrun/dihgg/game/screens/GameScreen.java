666851bbd921f6c0d9f98e45467f9759960b2b98
silmara <silmaraquel@ig.com.br>
2017-05-27 13:41:53 -0300

    Adicionado array de serras

--- core/src/com/pigletrun/dihgg/game/screens/GameScreen.java
+++ core/src/com/pigletrun/dihgg/game/screens/GameScreen.java
@@ -3,19 +3,23 @@ package com.pigletrun.dihgg.game.screens;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.utils.DragListener;
+import com.badlogic.gdx.utils.Array;
 import com.pigletrun.dihgg.game.components.characters.Pig;
 import com.pigletrun.dihgg.game.components.characters.Saw;
 import com.pigletrun.dihgg.game.components.ui.Hud;
 import com.pigletrun.dihgg.game.core.BaseScreen;
 import com.pigletrun.dihgg.game.core.GLOBAL;
 
-import static com.pigletrun.dihgg.game.core.GLOBAL.cam;
+import static com.pigletrun.dihgg.game.core.GLOBAL.SAW_COUNT;
+import static com.pigletrun.dihgg.game.core.GLOBAL.SAW_SPACING;
+import static com.pigletrun.dihgg.game.core.GLOBAL.SAW_WIDTH;
 
 class GameScreen extends BaseScreen {
 	// VARIÁVEIS
 	private Hud hud;
 	private Pig pig; // Porco
 	private Saw saw;
+	private Array<Saw> saws;
 
 	GameScreen(Game game) {
 		super(game); // chamar construtor pai
@@ -24,10 +28,14 @@ class GameScreen extends BaseScreen {
 		GLOBAL.ranking.setScore(0); // zerar o score
 
 		pig = new Pig(); // criar novo ator do porco
-		saw = new Saw((int) cam.viewportWidth);
+		saws = new Array<Saw>(); // cria array de serras
 
 		stage.addActor(pig); // Adicionar o porco ao cenário
-		stage.addActor(saw); // Adicionar a serra
+
+		//adiciona serras ao cenário
+		for (int i = 1; i <= SAW_COUNT; i++) {
+			stage.addActor(new Saw(i * (SAW_SPACING + SAW_WIDTH)));
+		}
 
 		// LISTENERS
 		// drag
@@ -46,12 +54,8 @@ class GameScreen extends BaseScreen {
 
 	@Override
 	public void render(float delta) {
-
 		hud.update();
 		pig.update();
-		saw.update();
-		if (saw.getX() < -50) {
-		}
 		super.render(delta);
 	}
 }
