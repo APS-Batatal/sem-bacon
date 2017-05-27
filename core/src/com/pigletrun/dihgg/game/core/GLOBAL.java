7f49dd01d912dc9d2efdb0465e672337a0ddad8f
silmara <silmaraquel@ig.com.br>
2017-05-27 13:23:49 -0300

    Adicionado mais algumas variáveis estaticas

--- core/src/com/pigletrun/dihgg/game/core/GLOBAL.java
+++ core/src/com/pigletrun/dihgg/game/core/GLOBAL.java
@@ -3,6 +3,9 @@ package com.pigletrun.dihgg.game.core;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 
 public class GLOBAL {
+	public static final int SAW_SPACING = 125; // distância lateral entre as serras
+	public static final int SAW_COUNT = 4;  // quantidade de serras do jogo
+	public static final int SAW_WIDTH = 52;
 	public static Ranking ranking = new Ranking();
 	public static OrthographicCamera cam = new OrthographicCamera();
 }
