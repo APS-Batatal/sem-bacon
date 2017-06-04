26d1c4548a91b2335e5e900c748358fcd030f0df
Diego Lopes <dihgg@dihgg.com>
2017-06-04 03:59:19 -0300

    Tela de Créditos

--- core/src/com/pigletrun/dihgg/game/Main.java
+++ core/src/com/pigletrun/dihgg/game/Main.java
@@ -1,11 +1,11 @@
 package com.pigletrun.dihgg.game;
 
 import com.badlogic.gdx.Game;
-import com.pigletrun.dihgg.game.screens.EndScreen;
+import com.pigletrun.dihgg.game.screens.IntroScreen;
 
 class Main extends Game {
 	@Override
 	public void create () {
-		this.setScreen(new EndScreen(this));
+		this.setScreen(new IntroScreen(this));
 	}
 }
--- core/src/com/pigletrun/dihgg/game/screens/CreditsScreen.java
+++ core/src/com/pigletrun/dihgg/game/screens/CreditsScreen.java
@@ -1,10 +1,51 @@
 package com.pigletrun.dihgg.game.screens;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.pigletrun.dihgg.game.components.Image;
+import com.pigletrun.dihgg.game.components.Text;
+import com.pigletrun.dihgg.game.components.ui.Button;
 import com.pigletrun.dihgg.game.core.BaseScreen;
 
 public class CreditsScreen extends BaseScreen {
-	public CreditsScreen(Game game) {
+	private Button backBtn;
+	private Image logo;
+	private Text text;
+	private String[] jobs = {"Programação", "Ilustração", "Sonoplastia", "Realização", "powered by"};
+	private String[] names = {"Diego Lopes / Silmara Raquel", "Diego Lopes", "Fulano de Tal", "Unicluster / UNIP", "LIBGDX"};
+
+	public CreditsScreen(final Game game) {
 		super(game);
+
+		logo = new Image("logo/game.png");
+		logo.setPosition(Gdx.graphics.getWidth() / 2 - logo.getWidth() / 2, Gdx.graphics.getHeight() - logo.getHeight() - 48);
+
+		String txt = "";
+		for (int i = 0; i < jobs.length; i++) {
+			txt += jobs[i] + " : " + names[i] + "\n\n";
+		}
+
+		text = new Text(txt, 20);
+		text.setPosition(24, logo.getY() - logo.getHeight() - 120);
+
+		backBtn = new Button(new Texture(Gdx.files.internal("images/ui/buttons/back.png")));
+		backBtn.setPosition(Gdx.graphics.getWidth() - backBtn.getWidth() - 24, 24);
+
+		backBtn.addListener(new InputListener() {
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+				game.dispose();
+				game.setScreen(new MenuScreen(game));
+				return false;
+			}
+		});
+
+
+		stage.addActor(logo);
+		stage.addActor(text);
+		stage.addActor(backBtn);
 	}
 }
